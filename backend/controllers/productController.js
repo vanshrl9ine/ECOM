b51b0f6074d2886a5109952a0a7ead562c25044f
Vansh <guptavansh67@gmail.com>
2024-03-27 13:38:41 +0530

    pagination works

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -61,6 +61,7 @@ const deleteProduct=AsyncHandler(async(req,res,next)=>{
 const getAllProducts=AsyncHandler(async(req,res)=>{
     
     const resultPerPage=5;
+    const productCount=await Product.countDocuments();
 
     const apiFeature=new ApiFeatures(Product.find(),req.query).search().filter().pagination(resultPerPage);
     const products=await apiFeature.query;
