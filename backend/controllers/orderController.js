8cb6e2dd9ab09c02e82a6da0e07566ce420894c8
Vansh <guptavansh67@gmail.com>
2024-04-01 19:05:20 +0530

    new orer

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,49 +1,64 @@
 import orderModel from "../models/orderModel.js";
 import ProductModel from "../models/productModel.js";
-const Order=orderModel;
-const Product=ProductModel
+const Order = orderModel;
+const Product = ProductModel
 import ErrorHandler from "../utils/errorHandler.js";
 import AsyncHandler from "../middlewares/catchAsyncerror.js";
 
 //create new order
 const newOrder = AsyncHandler(async (req, res, next) => {
     const {
-      shippingInfo,
-      orderItems,
-      paymentInfo,
-      itemsPrice,
-      taxPrice,
-      shippingPrice,
-      totalPrice,
+        shippingInfo,
+        orderItems,
+        paymentInfo,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
     } = req.body;
-    
+
     const order = await Order.create({
-      shippingInfo,
-      orderItems,
-      paymentInfo,
-      itemsPrice,
-      taxPrice,
-      shippingPrice,
-      totalPrice,
-      paidAt: Date.now(),
-      user: req.user._id,
+        shippingInfo,
+        orderItems,
+        paymentInfo,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+        paidAt: Date.now(),
+        user: req.user._id,
     });
-  
+
     res.status(201).json({
-      success: true,
-      order,
-    });
-  });
-  
-// get logginuserorder
-const getSingleOrder=AsyncHandler(async(req,res,next)=>{
-    const order=await Order.findById(req.params.id).populate("user","name email");
-    if(!order){
-        return next(new ErrorHandler("order not found with this id",404));
-    }
-    res.status(200).json({
-        success:true,
+        success: true,
         order,
     });
 });
-export {newOrder,getSingleOrder};
+
+// get particular us
+const getSingleOrder = AsyncHandler(async (req, res, next) => {
+    const order = await Order.findById(req.params.id).populate(
+        "user",
+        "name email"
+    );
+
+    if (!order) {
+        return next(new ErrorHandler("Order not found with this Id", 404));
+    }
+
+    res.status(200).json({
+        success: true,
+        order,
+    });
+});
+
+// get logged in user  Orders
+const myOrders = AsyncHandler(async (req, res, next) => {
+    const orders = await Order.find({ user: req.user._id });
+
+    res.status(200).json({
+        success: true,
+        orders,
+    });
+});
+export { getSingleOrder, newOrder ,myOrders};
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,7 +1,9 @@
 import express from "express";
 import {isAuthenticatedUser,authorizeRoles} from '../middlewares/auth.js';
-import { newOrder } from "../controllers/orderController.js";
+import { getSingleOrder, myOrders, newOrder } from "../controllers/orderController.js";
 
 const orderRouter=express.Router();
 orderRouter.route("/order/new").post(isAuthenticatedUser,newOrder);
+orderRouter.route("/order/:id").get(isAuthenticatedUser,getSingleOrder);
+orderRouter.route("/orders/me").get(isAuthenticatedUser,myOrders);
 export default orderRouter;
